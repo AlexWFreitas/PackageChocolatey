e514057b4fd454b3b0a16ae7ae7be3a65296b5f3
Alex Wong de Freitas <otaru1921@gmail.com>
2020-12-22 21:10:13 -0300

    Create choco.bat
    First setup

---
+++ choco.bat
@@ -0,0 +1,31 @@
+choco install 7zip
+choco install audacity
+choco install avidemux
+choco install cpu-z
+choco install crystaldiskinfo
+choco install discord
+choco install docker
+choco install epicgameslauncher
+choco install foxitreader
+choco install gimp
+choco install googledrive
+choco install handbrake
+choco install libreoffice-fresh
+choco install miktex
+choco install mpc-hc
+choco install notepadplusplus
+choco install paint.net
+choco install obs-studio
+choco install qbittorrent
+choco install retroarch
+choco install sharex
+choco install slack
+choco install steam
+choco install telegram
+choco install texmaker
+choco install vlc
+choco install vscode
+choco install windirstat
+choco install zoom
+
+:: Will have to install Blitz, League of Legends, Logitech Drivers, GPU Drivers, WSL 2, Ubuntu on WSL 
